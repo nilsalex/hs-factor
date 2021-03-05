ce6c8af0773dabcc6b99f624a92884281eadc2a7
Nils Alex <nils@famalex.de>
2021-03-05 16:58:25 +0100

    haddock documentation

--- src/Factor.hs
+++ src/Factor.hs
@@ -2,30 +2,44 @@
 
 module Factor
     ( factor
+    , factorShor
+    , periodNaive
+    , modExp
     ) where
 
 import System.Random
 
+-- | wrapper for Shor's algorithm
+--
+-- captures trivial cases
 factor :: RandomGen g => g -> Integer -> Integer
 factor g n
-  | n == 1    = 1
-  | even n    = 2
-  | otherwise = factorShor g n
+  | n == 1    = 1 -- easy!
+  | even n    = 2 -- easy!
+  | otherwise = factorShor g n -- run Shor's algorithm
 
+-- | Shor's algorithm
 factorShor :: RandomGen g => g -> Integer -> Integer
 factorShor g n
-    | gcDiv > 1 = gcDiv
-    | odd r     = factorShor g' n -- try again!
-    | b == -1   = factorShor g' n -- try again!
-    | otherwise = gcd (b+1) n
+    | gcDiv > 1 = gcDiv           -- we were lucky!
+    | odd r     = factorShor g' n -- try another guess
+    | b == -1   = factorShor g' n -- try another guess
+    | otherwise = gcd (b+1) n     -- we found a solution!
   where
     (guess, g') = randomR (2,n-1) g
     gcDiv       = gcd n guess
-    r           = findPeriodNaive n guess
+    r           = periodNaive n guess
     b           = modExp n guess (r `div` 2)
 
-findPeriodNaive :: Integer -> Integer -> Integer
-findPeriodNaive order base = go 1 base
+-- | the period \( r \) of an element \( a \in (\mathbb{Z}/n\mathbb{Z})^\times \)
+--
+-- @
+-- modExp n a (periodNaive n a) = 1
+-- @
+--
+-- (classically and naive, i.e. very slow!)
+periodNaive :: Integer -> Integer -> Integer
+periodNaive order base = go 1 base
   where
     go r acc
       | acc == 1  = r
@@ -33,6 +47,11 @@ findPeriodNaive order base = go 1 base
                         !r'   = r + 1
                     in go r' acc'
 
+-- | modular exponentiation by squaring
+--
+-- @
+-- modExp n b e = b^e ``mod`` n
+-- @
 modExp :: Integer -> Integer -> Integer -> Integer
 modExp n b = go b 1
   where
